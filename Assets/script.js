2abaa34a111e6c9b2c93abe8a650eb9cef7100d8
Jason Triemert <jaytriemert@gmail.com>
2023-04-18 14:36:07 -0500

    updated javascript to provide better instructions

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,15 @@
 var generateBtn = document.querySelector("#generate");
 var selectChar
 var charArray = []
-var upppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numericArray = ["0","1","2","3","4","5","6","7","8","9"];
-var specialArray = [" ","!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","^","_","`","{","|","}","~"];
+var upppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArray = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~"];
 var length = 0
-
+var lowercaseTrue;
+var uppercaseTrue;
+var numericTrue;
+var specialTrue;
 var passwordArray = []
 
 function writePassword() {
@@ -14,7 +17,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-function passwordLength (){
+function passwordLength() {
   var passwordLength = prompt("How many characters do you want your password? Passwords must be between 8 and 128 characters.")
   if (passwordLength > 7 || passwordLength < 129) {
     length = passwordLength
@@ -25,45 +28,53 @@ function passwordLength (){
 }
 
 passwordLength();
-function uppercase(){
+function uppercase() {
   var uppercaseAnsw = prompt("Do you want to use uppercase letters in your password? (Yes or No)")
-  var uppercaseTrue = Boolean(uppercaseAnsw == "Yes")
-  if(uppercaseTrue){
+  uppercaseTrue = Boolean(uppercaseAnsw == "Yes")
+  if (uppercaseTrue) {
     charArray = charArray.concat(upppercaseArray);
-    }
+  }
 }
 uppercase();
 
-function lowercase(){
+function lowercase() {
   var lowercaseAnsw = prompt("Do you want to use lowercase letters in your password? (Yes or No)")
-  var lowercaseTrue = Boolean(lowercaseAnsw == "Yes")
-  if(lowercaseTrue == true){
+  lowercaseTrue = Boolean(lowercaseAnsw == "Yes")
+  if (lowercaseTrue == true) {
     charArray = charArray.concat(lowercaseArray);
-    }
   }
+}
 lowercase();
 
-function numeric(){
+function numeric() {
   var numericAnsw = prompt("Do you want to use numbers in your password? (Yes or No)")
-  var numericTrue = Boolean(numericAnsw == "Yes")
-  if(numericTrue == true){
+  numericTrue = Boolean(numericAnsw == "Yes")
+  console.log(numericTrue);
+  if (numericTrue == true) {
     charArray = charArray.concat(numericArray);
-    }
   }
+}
 numeric();
 
-function special(){
+function special() {
   var specialAnsw = prompt("Do you want to use special charaters in your password? (Yes or No)")
-  var specialTrue = Boolean(specialAnsw == "Yes")
-  if(specialTrue == true){
+  specialTrue = Boolean(specialAnsw == "Yes")
+  if (specialTrue == true) {
     charArray = charArray.concat(specialArray);
-    }
   }
+}
 special();
 
+if(!uppercaseTrue && !lowercaseTrue && !numericTrue && !specialTrue){
+  console.log(uppercaseTrue, lowercaseTrue, numericTrue, specialTrue)
+  alert("You have to select at least one group of letters to use. Reload the page to try again. ")
+}
+else {
 
-function addACharacter () {
-  var charIndex = Math.floor(Math.random() * (charArray.length-1 - 0 + 1) + 0)
+}
+
+function addACharacter() {
+  var charIndex = Math.floor(Math.random() * (charArray.length))
   console.log(charArray)
   return charArray[charIndex]
 }
@@ -75,8 +86,7 @@ function generatePassword() {
   for (var i = length; i > 0; i--) {
     password += addACharacter();
   }
-  console.log(password)
   return password
 }
-
+alert("Your password is ready. Click Generate Password")
 generateBtn.addEventListener("click", writePassword);
